b664eaa44ba7fb81b848452e4203bf8940dbd980
BRISSY Axel <axelbrissy0804@gmail.com>
2024-02-15 12:13:31 +0100

    Debut tilemap + cameraLockScript

---
+++ Assets/Scipts/Camera/CameraLock.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLock : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x + offset.x, target.position.y + offset.y, target.position.z + offset.z);
+            transform.position = Vector3.SmoothDamp(targetPosition, targetPosition, ref velocity, 0.05f);
+        }
+    }
+
+}
