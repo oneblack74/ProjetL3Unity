8abf5b87f0fcdbc41d0712144b8fb67554211d56
DELCROIX Victor <37382189+LeadCreep@users.noreply.github.com>
2024-03-13 00:21:28 +0100

    Outline for interactable objects

--- Assets/Scipts/Inventory/ItemDefinition.cs
+++ Assets/Scipts/Inventory/ItemDefinition.cs
@@ -9,12 +9,14 @@ public class ItemDefinition : ScriptableObject
     [SerializeField] private int MAX_STACK;
     [SerializeField] private int ID;
     [SerializeField] private Sprite ICON;
+    [SerializeField] private bool IS_PLACABLE;
 
-    public ItemDefinition(int id, int maxStack, Sprite icon)
+    public ItemDefinition(int id, int maxStack, Sprite icon, bool isPlacable)
     {
         ID = id;
         MAX_STACK = maxStack;
         ICON = icon;
+        IS_PLACABLE = isPlacable;
     }
 
     public int getID
@@ -30,6 +32,11 @@ public class ItemDefinition : ScriptableObject
     public Sprite getIcon
     {
         get { return ICON; }
+    }
+
+    public bool getIsPlacable
+    {
+        get { return IS_PLACABLE;  }
     }
 }
 // }
